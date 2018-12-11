f5b23f3fed0a9d50313b6ba2e8da8eeb15e016d4
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2018-12-11 18:36:05 +0100

    Add files via upload

---
+++ matriz1_3.cpp
@@ -0,0 +1,47 @@
+//Matrices
+#include <iostream>
+using namespace std;
+main() {
+       int tablero [5][5];         
+       int fila;
+       int columna;                                             
+       int relleno;
+       char salida;
+       for (fila=0 ; fila <= 4; fila++){ 
+	   		if (fila%2 == 0){
+			  for (columna=0; columna <=4; columna++){
+	   			if (columna%2 == 0){
+	   				relleno = 0;
+	   				tablero [fila][columna] = relleno;
+	   	
+				   }
+				else{
+					relleno = 1;
+					tablero [fila][columna] = relleno;
+				
+				}
+	   			cout << tablero [fila][columna];
+	   		
+	   		
+			   }        
+			   }
+			else{
+				for (columna=0; columna <=4; columna++){
+	   			if (columna%2 == 0){
+	   				relleno = 1;
+	   				tablero [fila][columna] = relleno;
+	   	
+				   }
+				else{
+					relleno = 0;
+					tablero [fila][columna] = relleno;
+				
+				}
+	   			cout << tablero [fila][columna];
+			   }         
+			}
+	    		cout << "\n";
+   }
+	   cin >> salida;
+       return 0;
+}

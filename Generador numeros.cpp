270a067a149cf824c7a66f1c7278bf7a152297dc
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2018-12-20 09:42:16 +0100

    Add files via upload

---
+++ Generador numeros.cpp
@@ -0,0 +1,24 @@
+//generar 5 numeros al azar
+#include <iostream>                     //mete y saca informacion
+#include <cstdlib>                           //contiene muchas cosas, entre ellas el generador de numeros aleatorios
+#include <ctime>                                  //permite jugar con el tiempo
+using namespace std;
+int main (){
+    float lista [5];                             //Aqui guardo mis numeros 
+    int i;
+    int relleno;
+    char salida;
+    double seed;                                 //como un float pero con mas precision para definir la semilla (doble bits
+    seed = time (0);
+    srand (seed);
+    for (i=0; i<5; i++){                        //repaso la lista
+        lista [i] = rand()%6;                   //les doy un valor aleatorio
+        cout << lista [i]; cout << "\n";
+        
+        }
+    
+        
+    cin >> salida;
+    return 0;
+       
+}

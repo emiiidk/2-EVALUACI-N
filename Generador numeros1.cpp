0114b37f1b2ef2adc8179e56fb5aa81632ace538
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2019-01-10 09:19:09 +0100

    Update Generador numeros1.cpp

--- Generador numeros1.cpp
+++ Generador numeros1.cpp
@@ -1,4 +1,4 @@
-//generar 5 numeros al azar
+//generar matriz letras aleatorias (variante)
 #include <iostream>                    //mete y saca informacion
 #include <cstdlib>                           //contiene muchas cosas, entre ellas el generador de numeros aleatorios
 #include <ctime>                                  //permite jugar con el tiempo
@@ -7,9 +7,9 @@ int main (){
     int matriz[3][3];
     char letras[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int fil,col;
-    int pos_letra;                        //Aqui guardo mis numeros 
+    int pos_letra;                        
     char salida;
-    double semilla;                                 //como un float pero con mas precision para definir la semilla (doble bits
+    double semilla;                                 //como un float pero con mas precision para definir la semilla (doble bits)
     semilla = time (0);
     srand(semilla);
     for(fil=0; fil<3; fil++){

ccb39af4508344adce797b508fac2d29a8f4b97c
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2019-02-21 09:26:21 +0100

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> //libreria basica de C
+#include <iostream>
+
+using namespace std;
+int main() {
+    int x; x=3;
+    int y; y=5;
+    char salida;
+    
+    printf("x=%d y=%d",x,y); //Significa: escribe "x=" (texto plano), definiendo el dato x, luego me da x. Dejo como un hueco para que luego me devuelva el valor de cada uno
+    
+    cin >> salida;
+    
+    }

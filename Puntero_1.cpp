8bb62fdfbf866f8dcfe653b0aa25223dacf718f9
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2019-02-21 09:25:39 +0100

    Add files via upload

---
+++ Puntero_1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> //libreria basica de C
+#include <iostream>
+
+using namespace std;
+int main() {
+    int x; x=3;
+    int y; y=5;
+    char salida;
+    printf("%d",x); //Significa: escribe la x siendo esta un entero
+    printf("%d",y);
+    
+    cin >> salida;
+    
+    }

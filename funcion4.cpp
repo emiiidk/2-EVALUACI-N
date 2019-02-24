2eca2ee2b984235d19cf096858b145dff38c48ea
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2019-02-24 23:19:43 +0100

    Update funcion4.cpp

--- funcion4.cpp
+++ funcion4.cpp
@@ -1,41 +1,36 @@
 #include<iostream>
-#include<string>
+#include <conio.h> 
+#include <string.h> 
 using namespace std;
 
 int main(){
-    int num1;
-    int i,j;
-    char dias_semana[7][10];
-    char nombre_dia[10];
-    int respuesta;
-    char salir;
-    int numero;
-    cout<<"Vamos a introducir los dias de la semana";
-    for(i=0;i<=6;i++){
-          cout<<"\ndia "<<i+1<<": ";
-          cin>>nombre_dia;     
-          for(j=0;j<strlen(nombre_dia)+1;j++){
-               dias_semana[i][j]=nombre_dia[j];
-          }                 
-    }
-    //Mostramos la matriz
-    cout<<"\nDias semana: ";
-    for(i=0;i<=6;i++){
-      for(j=0;dias_semana[i][j]!='\0';j++){
-        cout<<dias_semana[i][j];
-      }
-      cout<<"\n";
-    }
-    cout << "\nIntroduce el numero del dia de la semana";
-    cin >> numero;
-    for(i=0;i<=6;i++){
-      for(j=0;dias_semana[i][j]!='\0';j++){
-        cout<<dias_semana[numero-1][j];
-      }
-
-}
-    cin >> salir;
-    return 0;
+	char base_datos [7][10];
+	char dia_semana [10];
+	int i; int j;
+	char salida;
+	int caca;
+	
+	for (i=0;i<=6;i++){
+		cout << "Dia " << i+1 << ": ";
+		cin >> dia_semana;
+		for (j=0;j<strlen(dia_semana)+1;j++){
+			base_datos [i][j] = dia_semana[j];
+		}
+	}	
+	for (i=0;i<=6;i++){
+			for (j=0;j<strlen(dia_semana)+1;j++){
+				cout << base_datos [i][j];
+		}
+	cout << "\n";
 }
 
-
+	cin >> caca;
+	cout << base_datos [caca-1];
+	
+	cin >> salida;
+	return 0;
+	
+	
+	
+	
+}
